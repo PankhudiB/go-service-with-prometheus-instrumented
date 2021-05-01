c24b3a8f2bf3abe4065cc6926dfdf68e896d76c2
Pankhudi <bpankhudi7@gmail.com>
2021-05-01 17:43:15 +0530

    Initialize gin router and add POST hello endpoint

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go-service/request"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Starting service A")
+	r := gin.Default()
+	r.POST("/hello", func(ctx *gin.Context) {
+		handleRequest(ctx)
+	})
+
+	err := http.ListenAndServe(":8090", r)
+	if err != nil {
+		fmt.Println("Could not start service", err)
+	}
+}
+
+func handleRequest(ctx *gin.Context) {
+	fmt.Println("------------------ Welcome ------------------")
+	var req request.HelloRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		fmt.Println("Error reading hello request", err.Error())
+		ctx.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	fmt.Println("Message :", req.Message)
+	ctx.Status(http.StatusOK)
+	return
 }
---
+++ request/HelloRequest.go
@@ -0,0 +1,5 @@
+package request
+
+type HelloRequest struct {
+	Message string `json:"message"`
+}
